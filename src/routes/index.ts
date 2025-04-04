82eb01d1b8699982c3e1d58383b1212dc2cd5655
taiwo akinsuyi <teeboidayo@gmail.com>
2025-04-04 17:15:39 +0100

    feat: get signal endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import routeNotFound from "../middleware/not-found";
 import authRoute from "../modules/auth/auth-routes";
+import signalRoute from "../modules/signal/signal-routes";
 
 const router = Router();
 
 
 router.use(authRoute);
+router.use(signalRoute)
 router.use(routeNotFound);
 
 export default router;
